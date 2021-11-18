e7edb5cb06f4c36cc803e2e6b03d039c13d076bf
mmgunes <93310164+mmgunes@users.noreply.github.com>
2021-11-18 15:46:00 +0300

    Drawer İşlemleri

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -6,18 +6,82 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
-import HomeScreen  from '../screens/Home';
+import HomeScreen from '../screens/Home';
+import IconMa from 'react-native-vector-icons/MaterialIcons';
+import IconFa from 'react-native-vector-icons/FontAwesome';
+import IconMaC from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
+const HomeScreenStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={{
+          title:'',
+          headerLeft: ()=> {} //Fonksiyon Image componentini çalıştıracak
+          }} />
+    </Stack.Navigator>
+  );
+};
+
+const MyDrawer = () => {
+  return (
+    <Drawer.Navigator
+      drawerStyle={{
+        backgroundColor: '#0271cd',
+      }}
+      drawerContentOptions={{
+        activeTintColor: '#fff',
+        inactiveTintColor: '#fff',
+        headerStyle: {
+          backgroundColor: '#0271cd',
+          borderBottomColor: '#fff',
+          borderBottomWidth: 1,
+        },
+        itemStyle: {marginVertical: 5},
+        labelStyle: {
+          fontSize: 18,
+        },
+      }}>
+      <Drawer.Screen
+        name="HomeScreen"
+        component={HomeScreenStack}
+        options={{
+          drawerLabel: 'Ana Sayfa',
+          drawerIcon: focused => {
+            <IconMa name="home" size={24} color="#fff" />;
+          },
+        }}
+      />
+
+      <Drawer.Screen
+        name="KampanyaScreen"
+        component={HomeScreenStack}
+        options={{
+          drawerLabel: 'Kampanyalar',
+          drawerIcon: focused => {
+            <IconMa name="campaign" size={24} color="#fff" />;
+          },
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
 export default function Routes() {
   return (
-    <View>
-        <Text>
-            Opet App Routes
-        </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MyDrawer"
+          component={MyDrawer}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 /*
@@ -57,4 +121,48 @@ return(içine yaz)
 
 <Stack.Screen //İlk Ekran
           name="Home"
-          component={HomeScreen} //Çağıracağı Prop */
+          component={HomeScreen} //Çağıracağı Prop 
+          
+ÖRNEK/*/
+/*
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from '../screens/Home';
+import FaktoriyelScren from '../screens/Faktoriyel';
+
+const Stack = createStackNavigator(); 
+const Drawer = createDrawerNavigator(); //Navigationları Drawer değişkenimizle çağıracağız
+		  
+		  const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false, //Geri butonu gözükmesin
+          }}></Stack.Screen>
+		  
+        <Stack.Screen
+          name="Faktoriyel"
+          component={FlexBoxScreen}
+          options={{
+            title: 'Faktoriyel Screen',
+            headerStyle: {backgroundColor: 'blue'},
+            headerTintColor: 'white',
+            headerTitleStyle: {fontWeight: 'bold'},
+          }}
+        />
+        ...
+		.<Stack.Screen/>..
+		...
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};*/
+
+//*
