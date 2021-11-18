9b186abf3c480ebf3d8db23ecd690655b5b87489
mmgunes <93310164+mmgunes@users.noreply.github.com>
2021-11-18 13:29:30 +0300

    Dosyalar Oluşturuldu

---
+++ navigation/Route.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Route() {
+    return (
+        <View>
+        <Text>Route Sayfası</Text>
+    </View>
+}
