9769f77a65cdc0ddd572ecb2c55ac81e95a406a5
mmgunes <93310164+mmgunes@users.noreply.github.com>
2021-11-22 15:19:40 +0300

    Body Kısmı 4 bölündü

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,8 +9,7 @@ import {
 } from 'react-native';
 
 import IconMa from 'react-native-vector-icons/MaterialIcons';
-import {deviceHeight,deviceWidth} from '../utils/Dimensions'
-
+import {deviceHeight, deviceWidth} from '../utils/Dimensions';
 
 const bgImage = {uri: 'https://van.bel.tr/dosya/slider/3.jpg'};
 //const deviceWidth = Dimensions.get('window').width; //Ekranın genişlik değeri
@@ -27,27 +26,76 @@ export default function HomeScreen({navigation}) {
             backgroundColor: '#e9bc18',
             // height: deviceHeight / 2,
           }}>
-            <Text>asdlja{deviceHeight}45564{deviceWidth}  aşskld</Text>
+          <Text>
+            asdlja{deviceHeight}45564{deviceWidth} aşskld
+          </Text>
           <Image
             style={{
-             
               resizeMode: 'cover',
               width: deviceWidth,
-              height: deviceHeight / 2 -70,
+              height: deviceHeight / 2 - 70,
             }}
-            
             source={{
               uri: 'https://reactnative.dev/img/tiny_logo.png',
             }}
           />
         </View>
-        <View style={{flex: 1, height: deviceHeight}}>
-          <Text>Body</Text>
+        <View style={{flex: 1, width: '100%', marginTop: 35}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderWidth: 1,
+                borderRightColor: 'rgba(255, 255, 255,0.2)',
+                borderBottomColor: 'rgba(255, 255, 255,0.2)',
+              }}>
+              <Text style={{color: 'white', fontSize: 24}}>En Yakın Opet</Text>
+              <Text style={{color: 'white', fontSize: 24}}>4,7 Km</Text>
+              
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRightWidth: 1,
+                borderBottomColor: 'rgba(255, 255, 255,0.2)',
+                borderBottomWidth:1
+              }}>
+              <Text style={{color: 'white', fontSize: 24}}>Akıllı Dolum</Text>
+              <IconMa name='local-gas-station' color='white' size={48}/>
+            </View>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRightWidth: 1,
+                borderRightColor: 'rgba(255, 255, 255,0.2)',
+              }}>
+              <Text style={{color: 'white', fontSize: 24}}>Benzin</Text>
+              <Text style={{color: 'white', fontSize: 24}}>9 TL</Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+               
+              }}>
+              <Text style={{color: 'white', fontSize: 24}}>Ödeme Yap </Text>
+              <IconMa name='payment' color='white' size={48}/>
+            </View>
+          </View>
         </View>
         <View
           style={{
-            position: 'absolute',
-            bottom: 0,
+            // position: 'absolute',
+            // bottom: 0,
             height: 60,
             //backgroundColor: '#e9bc18',
             borderTopWidth: 1,
