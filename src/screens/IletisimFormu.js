5c9a04640ffedfa356abbbff0cbb9530863195c7
mmgunes <93310164+mmgunes@users.noreply.github.com>
2021-11-24 09:04:18 +0300

    RN-elements ,RN-dropdown-picker, RN-smtpmailer paket yükledik ve mport işlemi

---
+++ src/screens/IletisimFormu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { CheckBox } from 'react-native-elements';
+import DropDownPicker from 'react-native-dropdown-picker';
+import RNSmtpMailer from 'react-native-smtp-mailer';
+
+const IletisimFormuScreen = () => {
+    return (
+        <View>
+            <Text>İletişim Formu  2</Text>
+        </View>
+    )
+}
+
+export default IletisimFormuScreen
+
